9aee5c405bd99ad5ab12f6c4ecd052d36278b664
MysticalRobot <dhruv21420@gmail.com>
2024-09-06 11:40:53 -0400

    W implementations

--- src/Flood.java
+++ src/Flood.java
@@ -11,6 +11,15 @@ public class Flood {
                               LinkedList<Coord> flooded_list,
                               Tile[][] tiles,
                               Integer board_size) {
+        LinkedList<Coord> flooded_list_copy = new LinkedList<>(flooded_list);
+        for (Coord coord : flooded_list_copy) {
+            for (Coord neighbor : coord.neighbors(board_size)) {
+                int x = neighbor.getX(), y = neighbor.getY();
+                if (!flooded_list_copy.contains(neighbor) && color == tiles[x][y].getColor()) {
+                    flooded_list.add(neighbor);
+                }
+            }
+        }
     }
 
     // An alternative implementation goes here.
@@ -18,7 +27,15 @@ public class Flood {
                              LinkedList<Coord> flooded_list,
                              Tile[][] tiles,
                              Integer board_size) {
-        // YOUR CODE
+        Set<Coord> flooded_set = new HashSet<>(flooded_list);
+        for (Coord coord : flooded_set) {
+            for (Coord neighbor : coord.neighbors(board_size)) {
+                int x = neighbor.getX(), y = neighbor.getY();
+                if (!flooded_set.contains(neighbor) && color == tiles[x][y].getColor()) {
+                    flooded_list.add(neighbor);
+                }
+            }
+        }
     }
 
 }
