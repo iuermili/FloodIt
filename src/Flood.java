eb3bebfae59bb0f39c3722164fb49fc30956a78e
LincolnFirks <lincfirks@gmail.com>
2024-09-06 11:14:54 -0400

    First implementation (Slow)

--- src/Flood.java
+++ src/Flood.java
@@ -11,7 +11,18 @@ public class Flood {
                               LinkedList<Coord> flooded_list,
                               Tile[][] tiles,
                               Integer board_size) {
-        // YOUR CODE
+        LinkedList<Coord> flooded_list_copy = new LinkedList<>(flooded_list);
+        for (Coord coord : flooded_list_copy){
+            List<Coord> neighbors = coord.neighbors(board_size);
+            for (Coord neighbor : neighbors) {
+                if (tiles[neighbor.getY()][neighbor.getX()].getColor() == color){
+                    if (!flooded_list_copy.contains(neighbor)) {
+                        flooded_list.add(neighbor);
+                    }
+                }
+            }
+
+        }
     }
 
     // An alternative implementation goes here.
